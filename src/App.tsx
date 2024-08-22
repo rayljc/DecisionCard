f7dc406674f4105889c7f9416d020246d4b1f169
HLHsieh <86452106+HLHsieh@users.noreply.github.com>
2024-08-22 16:47:25 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,29 +15,17 @@ export interface QuestionData {
 }
 
 const questions: QuestionData[] = [
-  { id: 1, content: '>= 30x dep', optionOneText: "Yes", optionOneNextId: 2, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: false },
-  { id: 2, content: "Sequencing from R10 or R9", optionOneText: "R10", optionOneNextId: 3, optionTwoText: "R9", optionTwoNextId: 4, optionThreeText: "", optionThreeNextId: -1, finalState: false },
-  { id: 3, content: 'Know total length of the TR region?', optionOneText: "Yes", optionOneNextId: 5, optionTwoText: "No", optionTwoNextId: 6, optionThreeText: "", optionThreeNextId: -1, finalState: false },
-  { id: 4, content: 'Know total length of the TR region?', optionOneText: "Yes", optionOneNextId: 7, optionTwoText: "No", optionTwoNextId: 8, optionThreeText: "", optionThreeNextId: -1, finalState: false },
-  { id: 5, content: 'Is the total length < 20000 bp?', optionOneText: "Yes", optionOneNextId: 9, optionTwoText: "No", optionTwoNextId: 10, optionThreeText: "", optionThreeNextId: -1, finalState: false },
-  { id: 6, content: 'TR Type?', optionOneText: "Macrosatellite", optionOneNextId: 11, optionTwoText: "STR or VNTR", optionTwoNextId: 12, optionThreeText: "", optionThreeNextId: -1, finalState: false },
-  { id: 7, content: 'Is the total length < 20000 bp?', optionOneText: "Yes", optionOneNextId: 13, optionTwoText: "No", optionTwoNextId: 14, optionThreeText: "", optionThreeNextId: -1, finalState: false },
-  { id: 8, content: 'TR Type?', optionOneText: "VNTR", optionOneNextId: 15, optionTwoText: "SNTR", optionTwoNextId: 16, optionThreeText: "Macrosatellite", optionThreeNextId: 17, finalState: false },
-  { id: 9, content: 'Is TR Type a Macrosatellite?', optionOneText: "Yes", optionOneNextId: 18, optionTwoText: "No", optionTwoNextId: 19, optionThreeText: "Unknown", optionThreeNextId: 20, finalState: false },
-  { id: 10, content: 'STRaglr', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
-  { id: 11, content: 'STRaglr', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
-  { id: 12, content: 'NanoRepeat, STRaglr, Tandem-genotypes', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
-  { id: 13, content: 'Is TR Type a Macrosatellite?', optionOneText: "Yes", optionOneNextId: 21, optionTwoText: "No", optionTwoNextId: 22, optionThreeText: "Unknown", optionThreeNextId: 23, finalState: false },
-  { id: 14, content: 'STRaglr', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
-  { id: 15, content: 'NanoRepeat, STRaglr, Tandem-genotypes', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
-  { id: 16, content: 'NanoRepeat, STRaglr', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
-  { id: 17, content: 'STRaglr', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
-  { id: 18, content: 'STRaglr, Tandem-genotypes', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
-  { id: 19, content: 'Tandem-genotypes, NanoRepeat, STRaglr', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
-  { id: 20, content: 'STRaglr', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
-  { id: 21, content: 'STRaglr', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
-  { id: 22, content: 'NanoRepeat, STRaglr', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
-  { id: 23, content: 'STRaglr', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
+  { id: 1, content: 'Interest in a specific TR?', optionOneText: "Yes", optionOneNextId: 2, optionTwoText: "No", optionTwoNextId: 3, optionThreeText: "", optionThreeNextId: -1, finalState: false },
+  { id: 2, content: "What kind of analysis?", optionOneText: "Quantification", optionOneNextId: 4, optionTwoText: "Genotyping", optionTwoNextId: 5, optionThreeText: "DNA fingerprinting", optionThreeNextId: 6, finalState: false },
+  { id: 3, content: 'STRaglr, RepeatHMM, TRF', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
+  { id: 4, content: 'Which type are you interested in?', optionOneText: "STR (2-6 bp)", optionOneNextId: 7, optionTwoText: "VNTR (6-100 bp)", optionTwoNextId: 8, optionThreeText: "Macrosatellites (>100 bp)", optionThreeNextId: 9, finalState: false },
+  { id: 5, content: 'Which type are you interested in?', optionOneText: "STR (2-6 bp)", optionOneNextId: 10, optionTwoText: "VNTR (6-100 bp)", optionTwoNextId: 11, optionThreeText: "Macrosatellites (>100 bp)", optionThreeNextId: 9, finalState: false },
+  { id: 6, content: 'NASTRA', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
+  { id: 7, content: 'tandem-genotypes, NanoRepeat, Straglr, LongTR, HMMSTR', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
+  { id: 8, content: 'HMMSTR, Straglr, tandem-genotypes, NanoRepeat', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
+  { id: 9, content: 'Straglr, LongTR', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
+  { id: 10, content: 'NanoRepeat, HMMSTR', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true },
+  { id: 11, content: 'tandem-genotypes, HMMSTR', optionOneText: "", optionOneNextId: -1, optionTwoText: "", optionTwoNextId: -1, optionThreeText: "", optionThreeNextId: -1, finalState: true }
 ];
 
 interface QuestionProps {
